940975528726ac942e204bc8cba5510b5b19a927
tarasiegel <tsiegel@thrillist.com>
2015-05-08 18:40:16 -0400

    switching to templates ejs

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
+var express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    swig = require('swig');
 
 app.use(express.static(__dirname + '/public'));
+app.engine('html', swig.renderFile);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+swig.setDefaults({ cache: false });
+app.set('view cache', false);
+swig.setDefaults({ cache: false });
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.render('pages/index');
 });
 
 app.get('/about', function(req, res){
-  res.sendFile(__dirname + '/public/about.html');
+  res.render('pages/about');
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
