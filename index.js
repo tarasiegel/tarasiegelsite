8a854ce3d5d040cd7ba94ed0557c5166c1d18230
tarasiegel <tsiegel@thrillist.com>
2016-01-12 14:57:29 -0500

    updates to where i will be reading in info;

--- index.js
+++ index.js
@@ -12,6 +12,32 @@ swig.setDefaults({ cache: false });
 app.set('view cache', false);
 swig.setDefaults({ cache: false });
 
+
+
+var jsonObj = require("./public/js/data.json");
+
+var designData = jsonObj.design;
+var designData = Object.keys(designData).map(function (key) {return designData[key]});
+
+var codeData = jsonObj.design;
+var codeData = Object.keys(designData).map(function (key) {return designData[key]});
+
+// Parse.initialize("7TmB0Eg1i2GPrE6wYAhzCjyeTAUeu7HhcKTawhSk", "1zg8wOld5UyYXqozyTcIIkZI8yWwY0MlT7cH2G6q");
+
+// var designInfo = Parse.Object.extend("design");
+// var query = new Parse.Query(designInfo);
+// var designData = {};
+// query.find({
+//   	success: function(results) {
+//   		console.log(results);
+//   		designData = results;
+// 	},
+// 	error: function(error) {
+// 		console.log("Error: " + error.code + " " + error.message);
+// 	}
+// });
+
+
 app.get('/', function(req, res){
   res.render('pages/index');
 });
@@ -20,11 +46,14 @@ app.get('/about', function(req, res){
   res.render('pages/about');
 });
 
-app.get('/art', function(req, res){
-  res.render('pages/art', {data: ''});
+app.get('/design', function(req, res){
+	res.render('pages/design', { designData: designData});
+});
+
+app.get('/code', function(req, res){
+	res.render('pages/code', { codeData: codeData});
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
--- public/js/default.js
+++ public/js/default.js
@@ -1,60 +1,14 @@
 
-Parse.initialize("6Wjb9qN60rilr4LKChBlxt1lNh1eMCI8InP31mPZ", "icCZ3TUR1UGNrgbZcFWeQ9N1WrrMdY0uEG1KFDJK");
 
 $(document).ready(function() {
-	headerAnimation();
+
+	// $('.background-image img').css('height', $(window).height());
+
+	// window.addEventListener('resize', function() {
+	// 	$('.background-image img').css('height', $(window).height());
+	// }, true);
 
 });
 
-function headerAnimation() {
-	var controller = $.superscrollorama();
-	controller.addTween('#scroll-point',
-	    (new TimelineLite())
-	    .append([
-	      // Move Entire Logo
-	      TweenMax.fromTo($('#ts-logo'), .5,
-	        {
-	        	css:{ top: '-32px' },
-	        	ease:Quad.easeInOut,
-	        	immediateRender:true
-	        },
-	        {
-	        	css:{ top: '-64px' },
-	        	ease:Quad.easeInOut
-	        }),
-	      // Move Logo Background
-	      TweenMax.fromTo($('#logo-diamond'), .5,
-	        {
-	        	css:{ scale: 1 },
-	        	ease:Expo.easeOut,
-	        	immediateRender:true},
-	        {
-	        	css:{ scale: .65 },
-	        	ease:Quad.easeInOut
-	        }),
 
-	      // Move Inside Logo Graphic
-	      TweenMax.fromTo($('#logo-big'), .25,
-	        {
-	        	css:{opacity: 1},
-	        	immediateRender:true
-	        },
-	        {
-	        	css:{opacity: 0}
-	        }),
-	      // Move TS
-	      TweenMax.fromTo($('#logo-small'), .4,
-	        {
-	        	css:{ opacity: 0 },
-	        	ease:Expo.easeInOut,
-	        	immediateRender:true},
-	        {
-	        	css:{ opacity: 1 },
-	        	ease:Expo.easeInOut, delay:.15
-	        })
-	    ]),
-	    0,
-	  	0
-	);
-};
 
