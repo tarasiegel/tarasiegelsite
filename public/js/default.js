7e6e04214237cdc2ef745878e12de98fda858878
tarasiegel <tsiegel@thrillist.com>
2017-03-31 23:58:55 -0400

    expand

--- public/js/default.js
+++ public/js/default.js
@@ -8,6 +8,19 @@ $(document).ready(function() {
 	// 	$('.background-image img').css('height', $(window).height());
 	// }, true);
 
+	$('.js-portfolio-item').click(function() {
+		var $this = $(this);
+
+		var currentLi = $this.closest('.portfolio__item');
+
+		if (currentLi.hasClass('item-closed')) {
+			currentLi.removeClass('item-closed');
+		} else {
+			currentLi.addClass('item-closed');
+		}
+
+	});
+
 });
 
 
